d3239e29c7cac6ece5ea402e44351c49d1f89d8c
Jeffin <jeffinmathew00@gmail.com>
2021-08-31 16:41:13 +0530

    first commit

---
+++ eclipsetextcreation/numberExcel.java
@@ -0,0 +1,67 @@
+package com.number.excel;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class numberExcel {
+	private static List<printExcel> printexcel= new ArrayList<printExcel>();
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int digit;
+		
+			
+			Workbook workbook= new XSSFWorkbook();
+			Sheet sheet=workbook.createSheet("printexcel");
+		
+				
+			
+			System.out.println("enter any number");
+			Scanner scan=new Scanner(System.in);
+			digit=scan.nextInt();
+			printExcel number=new printExcel(digit);
+			for(int i=0;i<10;i++) {
+				Row headerRow =sheet.createRow(i);
+				Cell cell =headerRow.createCell(0);
+		
+				cell.setCellValue(digit);
+			
+				
+			}
+				
+					
+			PrintWriter out = new PrintWriter("E:\\printnumber.txt"); 
+			for(int j=0;j<10;j++) {
+	          
+	         out.println(digit);
+	          
+			}
+			 out.close(); 
+
+			File yourFile = new File("E://printexcel1.xlsx");
+			yourFile.createNewFile();
+			FileOutputStream fileOut= new FileOutputStream(yourFile, false);
+			workbook.write(fileOut);
+			fileOut.close();
+			workbook.close();
+			
+			
+	}
+		
+
+	}
+
+
